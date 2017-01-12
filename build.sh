93f9a5b2710b51a56f7b9eb45babff50292505fd
Alexander NeonXP Kiryukhin <alexander@kiryukhin.su>
2017-01-12 23:43:58 +0300

    Initial

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+echo "Building Telegram theme"
+zip build.zip colors.tdesktop-theme tiled.png
+mv build.zip Darcula.NeonXP.v1.tdesktop-theme
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Darcula Telegram Desktop Theme
+## By Alexander NeonXP Kiryukhin
+
+Theme channel: [@DarculaColorTheme](https://t.me/DarculaColorTheme)
